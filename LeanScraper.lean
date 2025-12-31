55029ca4bf0a03120991998d6f9d7dc5ef42d485
Malhar A. Patel <malharalpeshpatel@gmail.com>
2025-12-31 06:55:31 +0000

    Refactor code for setting inputs and include "info" in the output json

--- LeanScraper.lean
+++ LeanScraper.lean
@@ -6,13 +6,22 @@ import LeanScraper.Printercept
 import Lean
 import Init.System
 
-set_option synthInstance.maxHeartbeats 1000000
-
-set_option maxRecDepth 1000
-set_option compiler.extract_closed false
+set_option synthInstance.maxHeartbeats 10000000000
+set_option maxRecDepth 100000
+--set_option compiler.extract_closed false
 
 open Lean Elab Command Meta
 
+/-
+Set the modules you want to extract from
+-/
+def modules : Array Import :=
+  #[{module := `Mathlib}]
+
+/-
+Set whatever you want to extract to `true`,
+and the rest to `false`.
+-/
 def checkUseful (info : ConstantInfo) : Bool :=
   match info with
   | .axiomInfo _  => false
@@ -23,6 +32,12 @@ def checkUseful (info : ConstantInfo) : Bool :=
   | .inductInfo _ => true
   | .ctorInfo _   => false
   | .recInfo _    => false
+
+/-
+Set the output (.jsonl) path for the data
+-/
+def dataOutFilePath : String :=
+  "Data/mathlib.jsonl"
 
 def infoToString (info : ConstantInfo) : String :=
   match info with
@@ -35,70 +50,49 @@ def infoToString (info : ConstantInfo) : String :=
   | .ctorInfo _   => "ctor"
   | .recInfo _    => "rec"
 
-def constantNames : CommandElabM <| Array Name := do
+def constantDecls : CommandElabM <| Array <| Name × ConstantInfo := do
   let env ← getEnv
-  IO.println "got env"
+  IO.println "Environment Loaded!"
   let decls := env.constants.map₁.toArray
-  IO.println s!"got decls : {decls.size}"
+  IO.println s!"Number of total Decls : {decls.size}"
   let filtered_decls := decls.filter <| fun (_,info) => checkUseful info
-  let filtered_decls_names := filtered_decls.map <| fun (name, _) => name
-  return filtered_decls_names
-
--- TODO:
--- Include constantInfo type as well in the Json
+  return filtered_decls
 
 def writeDocs (outFilePath : String) : CommandElabM Unit := do
   -- Get all name values
-  let names ← constantNames
-  --let names := #[``Nat.div_pos_iff._simp_1]
-  IO.println s!"got names : {names.size}"
+  let decls ← constantDecls
+  IO.println s!"Number of filtered Names : {decls.size}"
   -- Get the print object for each name in names
-  let printObjs ← names.mapM <| fun name => do
+  let printJsonObjs ← decls.mapM <| fun (name,info) => do
     return Json.mkObj [
       ("name", .str name.toString),
+      ("info", .str <| infoToString info),
       ("print", .str <| ← printerceptName name)]
   let handle ← IO.FS.Handle.mk outFilePath IO.FS.Mode.write
   -- Write each object to `outFilePath`
-  for obj in printObjs do
+  for obj in printJsonObjs do
     handle.putStrLn obj.compress
   handle.flush
   IO.println s!"Content successfully written to {outFilePath}"
 
--- #eval writeDocs "test.jsonl"
-
 def writeDocsCore (outFilePath : String) : CoreM Unit := do
   liftCommandElabM <| writeDocs outFilePath
-
-#print EIO
-
-def EIO.runToIO' (eio : EIO Exception α) : IO α  := do
-  match ← eio.toIO' with
-  | Except.ok x =>
-      pure x
-  | Except.error e =>
-      let msg ← e.toMessageData.toString
-      IO.throwServerError msg
-
--- #eval printerceptName ``Nat.div_pos_iff._simp_1
 
 unsafe def main : IO Unit := do
   initSearchPath (← Lean.findSysroot) [
     "build/lib",
     "lake-packages/mathlib/build/lib/",
-    -- "lake-packages/std/build/lib/",
-    -- "lake-packages/Qq/build/lib/",
-    -- "lake-packages/aesop/build/lib/",
-    -- "lake-packages/proofwidgets/build/lib"
+    "lake-packages/std/build/lib/",
+    "lake-packages/Qq/build/lib/",
     ]
   withImportModules
-    #[{module := `Lean}]
+    modules
     {entries := []}
   <| fun env => do
     let coreCtx : Core.Context :=
       {fileName := "", fileMap := {source:= "", positions := #[]},
-        maxHeartbeats := 1000000000, maxRecDepth := 10000}
-    let outFilePath := "test.jsonl"
-    let eio := writeDocsCore outFilePath |>.run' coreCtx {env := env}
+        maxHeartbeats := 10000000000, maxRecDepth := 100000}
+    let eio := writeDocsCore dataOutFilePath |>.run' coreCtx {env := env}
     match ← eio.toIO' with
     | Except.ok x => pure x
     | Except.error e =>
